559d6ea4b76dfffe21ab11f56cf490f1514c8b36
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2022-05-27 06:23:27 +0500

    change config

--- app/db/datasource.ts
+++ app/db/datasource.ts
@@ -1,13 +1,19 @@
 import { DataSource } from 'typeorm';
 import {User, Role, User_details, Task} from './entities';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const PostgresDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "lol355183",
-    database: "postgres",
+    host: process.env.db_host,
+    port: +process.env.db_port,
+    username:  process.env.db_username,
+    password: process.env.db_password,
+    database: process.env.dbatabse,
+    // ssl: {
+    //     rejectUnauthorized:false
+    // },
     entities: [User, Role, User_details, Task],
 })
 
--- server.ts
+++ server.ts
@@ -3,9 +3,12 @@ import Config from './app/config/app.config'
 import DataBase from './app/db/datasource';
 import Router from './app/router';
 import * as bodyParser from 'body-parser'
+import * as cors from 'cors';
 
 DataBase.initialize().then(() => console.log('asds'))
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/api',Router);
